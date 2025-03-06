c23a8906c82546a87542c4691742bc2f27459084
SrGalleta444 <cnm.arielanaya@gmail.com>
2025-03-06 14:04:07 -0600

    Create Ejercicio 1.c

---
+++ Ejercicio 1.c
@@ -0,0 +1,14 @@
+// Ariel Anaya
+//Ejercicio 1 
+#include<stdio.h>
+#include<stdlib.h>
+intmain()
+{
+    int a = 3;
+    int b = 2;
+    if(a > b)
+    { 
+        printf("a es mayor que b \n");
+    }
+    return 0;
+}
